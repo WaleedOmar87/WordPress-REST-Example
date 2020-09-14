f6dca071a1eaff3e408bbd3ff85d91a68259f8c8
waleed-omar <waleed@waleeds-MacBook-Pro.local>
2020-09-14 11:47:25 +0200

    update

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded' , () => {
+document.addEventListener("DOMContentLoaded", () => {
 	/*
 		This endpoint is uses to retrieve elementor blocks content from certain page based on page id
 		@var website : wordpress live site
 		@var pageID : the page id to retrieve elementor blocks content from
 	*/
-	const website = 'http://localhost:8888/wordpress_test';
+	const website = "http://localhost:8888/wordpress_test";
 	const pageID = 256;
-	const WPEndpoint = 'http://localhost:8888/wordpress_test/wp-json/CRB_Test_Block/v1/pages/256/getElementorContent';
+	const WPEndpoint = `${website}/wp-json/CRB_Test_Block/v1/pages/${pageID}/getElementorContent`;
 
 	/*
 		A Simple fetch request to the server
 	*/
 	const getPosts = fetch(WPEndpoint);
-	getPosts.then(res => {
-		console.log(res)
-	})
+	getPosts
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data);
+		});
 });
